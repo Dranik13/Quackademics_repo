fefcaa3295a729ea602669bfa37497f2276cb140
Robin Mann <robinmann@freenet.de>
2025-07-02 18:49:13 +0200

    Parking mode eingefügt

--- packages/followlane/src/switch_control_node.py
+++ packages/followlane/src/switch_control_node.py
@@ -10,7 +10,9 @@ from duckietown.dtros import DTROS, NodeType
 class ControlType(Enum):
     Lane = 1
     Obstacle = 2
-    Parking = 3
+    Intersection = 3
+    Parking = 4
+
 
 class SwitchControlNode(DTROS):
     def __init__(self,node_name):
@@ -20,27 +22,40 @@ class SwitchControlNode(DTROS):
         self._vehicle_name = os.environ['VEHICLE_NAME']
         self.sub_duckie = rospy.Subscriber(f"/{self._vehicle_name}/detect/duckie", Bool, self.cbDuckieDetected, queue_size = 1)
         self.sub_lane = rospy.Subscriber(f"/{self._vehicle_name}/detect/lane", Float64, self.cbLaneDetected, queue_size = 1)
+        self.sub_parking = rospy.Subscriber(f"/{self._vehicle_name}/detect/parking_active", Bool, self.cbParkingActive, queue_size=1)
         self.pub_control = rospy.Publisher(f"/{self._vehicle_name}/switch/control", Int32, queue_size = 1)
         self.sub_Obstacle_enabled = rospy.Subscriber(f"/{self._vehicle_name}/obstacle/enabled", Bool, self.cbObstacleEnabled, queue_size = 1)
         self._control_mode = ControlType.Lane   # start mode == Lane
         self._Obstacle_enabled = False
         self.counter = 0
+        self._parking_active = False
 
     def cbObstacleEnabled(self, msg):
         self._Obstacle_enabled = msg.data
-
+    
     def cbDuckieDetected(self, msg):
-        # Change Mode to Duckie if Duckie is detected and lock it for X time?
+        if self._parking_active:
+            return
         if msg.data:
             self._control_mode = ControlType.Obstacle
-        elif self._Obstacle_enabled == False:
+        elif not self._Obstacle_enabled:
             self._control_mode = ControlType.Lane
 
     def cbLaneDetected(self, msg):
-        # Change control Mode if Lane Detected and no Duckie
-        if msg.data > 0 and self._Obstacle_enabled == False:
+        if self._Obstacle_enabled or self._parking_active:
+            return
+        if msg.data > 0:
             self._control_mode = ControlType.Lane
-        
+
+    def cbParkingActive(self, msg):
+        self._parking_active = msg.data
+        if msg.data:
+            rospy.loginfo("🔐 Parking-Modus aktiviert.")
+            self._control_mode = ControlType.Parking
+        else:
+            rospy.loginfo("🔓 Parking-Modus deaktiviert.")
+            self._control_mode = ControlType.Lane
+
 
     def run(self):
         rate = rospy.Rate(10)
@@ -49,6 +64,7 @@ class SwitchControlNode(DTROS):
             msg_control = Int32()
             msg_control.data = self._control_mode.value
             self.pub_control.publish(msg_control)
+            #rospy.loginfo(f"Control mode: {self._control_mode.name} (Value: {msg_control.data})")
             rate.sleep()
             
 
